0b9a33bbc0502ea0466eb767e0e1af62b8347cd4
Varun Gole <varungole1999@gmail.com>
2025-01-23 23:25:39 +0530

    load balancer

--- loadbalancer/src/main/java/loadbalancer/AvailableServers.java
+++
@@ -1,9 +0,0 @@
-package loadbalancer;
-
-public class AvailableServers {
-    public final int PORT;
-
-    public AvailableServers(int PORT) {
-        this.PORT = PORT;
-    }
-}
--- loadbalancer/target/classes/loadbalancer/test.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..100000}
-do
-  curl -X GET http://localhost:1010 --data "Request $i"
-  echo "Request $i sent"
-done
